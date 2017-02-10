16d2da4afd2502ef7b9c21800b60e54c7ad46063
gitpetr <pkhilenko@yandex.ru>
2017-02-10 19:42:22 +0600

    add and remove cards

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,18 @@
 class Card < ApplicationRecord
+  validates :original, :translated, :review, presence: true 
+  validates :original, :translated, uniqueness: { case_sensitive: false }
+
+  before_create do
+    self.review = self.created_at +  86400 * 3
+  end
+
+  before_validation  :no_equal
+
+  protected
+
+  def no_equal
+    if self.original.downcase == self.translated.downcase
+      self.errors.add( :original, 'Оригинальный и переведённый тексты не должны быть равны друг другу')
+    end
+  end
 end
