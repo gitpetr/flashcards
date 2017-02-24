2d42f93fc308f10c7fdcc61b1fa3848da4acb034
gitpetr <pkhilenko@yandex.ru>
2017-02-24 17:58:13 +0600

    add clik_link in test have_selector pack, option  amb

--- spec/features/pack_spec.rb
+++ spec/features/pack_spec.rb
@@ -6,7 +6,7 @@ RSpec.feature "Cards", type: :feature do
   before(:each) do 
     user = create(:user, email: "test@test.test", password: "test", password_confirmation: "test") 
     pack =  create(:pack, user_id: user.id) 
-    pack1 = create(:pack, name: 'technic', user_id: user.id) 
+    pack1 = create(:pack, name: 'amb', user_id: user.id) 
     create(:card, user_id: user.id, pack_id: pack.id).update_attributes(review: -3.days.from_now)
     visit root_path
     login("test@test.test", "test" )
@@ -17,7 +17,7 @@ RSpec.feature "Cards", type: :feature do
       visit new_card_path
       fill_in :card_original, :with => "a"
       fill_in :card_translated, :with => "az"
-      select('technic', from: 'card_pack_id')
+      #select('techical', from: 'card_pack_id')
       attach_file 'card_image', "#{Rails.root}/spec/images/ten.jpg"
       click_button "Create Card"
       Card.last.update_attributes(review: -3.days.from_now)
@@ -27,16 +27,17 @@ RSpec.feature "Cards", type: :feature do
   end
 
    feature "have selector pack1" do 
-    it "have_selector pack, option  technic " do
+    it "have_selector pack, option  amb " do
       visit new_card_path
       fill_in :card_original, :with => "atr"
       fill_in :card_translated, :with => "azr"
-      select('technic', from: 'card_pack_id')
+      select('amb', from: 'card_pack_id')
       attach_file 'card_image', "#{Rails.root}/spec/images/ten.jpg"
       click_button "Create Card"
       Card.last.update_attributes(review: -3.days.from_now)
       visit root_path
-      expect(page).to  have_content "Pack: techical"
+      click_link "amb"
+      expect(page).to  have_content "Pack: amb"
     end
   end
 
