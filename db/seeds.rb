ec2ad2429f2a4c261608430130aadc18b3baea0f
pkhilenko <pkhilenko@yandex.ru>
2017-11-08 10:19:45 +0600

    complete

---
+++ db/seeds.rb
@@ -0,0 +1,23 @@
+# This file should contain all the record creation needed to seed the database
+# with its default values.
+# The data can then be loaded with the rake db:seed
+# (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'nokogiri'
+require 'open-uri'
+
+user = User.create(email: 'email@example.ru', password: '1234', password_confirmation: '1234')
+block = user.blocks.create(title: 'Карточка №1')
+
+doc = Nokogiri::HTML(open('https://www.learnathome.ru/blog/100-beautiful-words'))
+
+doc.search('//table/tbody/tr').each do |row|
+  original = row.search('td[2]')[0].content.downcase
+  translated = row.search('td[4]')[0].content.downcase
+  block.cards.create(original_text: original, translated_text: translated, user_id: user.id)
+end
